working wrong results

md/mdclient_h5.py
@@ -10,24 +10,31 @@ from mdcliapi2 import MajorDomoClient
 import zio
 import json
 import numpy as np
+import h5py
+
+import h5_utils as h5u
 
 def main():
     verbose = '-v' in sys.argv
     client = MajorDomoClient("tcp://localhost:5555", verbose)
+    fn = "test/data-0.h5"
+    im_tags = ['frame_loose_lf0', 'frame_mp2_roi0', 'frame_mp3_roi0']    # l23
     requests = 1
     for i in range(requests):
+        img = h5u.get_hwc_img(fn, i, im_tags, [1, 10], [800, 1600], [0, 6000], 4000) # V
         try:
-            label = json.dumps({"TENS":[{"dtype":'f',"part":1,"shape":[3,2],"word":4}]})
-            payload = np.array([[0, 1], [2, 3], [4, 5]], dtype='f')
+            label = json.dumps({"TENS":[{"dtype":'f',"part":1,"shape":img.shape,"word":4}]})
+            # payload = np.array([[0, 1], [2, 3], [4, 5]], dtype='f')
             m = zio.Message(form='TENS', label=label, 
                  level=zio.MessageLevel.warning,
-                 payload=[payload.tobytes()])
+                 payload=[img.tobytes()])
             print(m)
             client.send(b"torch", m.toparts())
         except KeyboardInterrupt:
             print ("send interrupted, aborting")
             return
-
+    
+    out = h5py.File("out.h5", "w")
     count = 0
     while count < requests:
         try:
@@ -43,8 +50,9 @@ def main():
             label = json.loads(m.label)
             shape = label["TENS"][0]["shape"]
             payload = m._payload[0]
-            ret = np.frombuffer(payload, dtype='f').reshape(shape)
-            print('reply:\n', ret)
+            mask = np.frombuffer(payload, dtype='f').reshape(shape)
+            dset= out.create_dataset("/%d/mask"%count, mask.shape, dtype='f')
+            print('reply:\n', mask.shape)
         count += 1
     print ("%i requests/replies processed" % count)
 

md/mdworker_ts.py
@@ -10,10 +10,13 @@ from mdwrkapi import MajorDomoWorker
 import zio
 import json
 import numpy as np
+import torch
 
 def main():
     verbose = '-v' in sys.argv
     worker = MajorDomoWorker("tcp://localhost:5555", b"torch", verbose)
+    model = "ts-model/unet1024-l23-cosmic500-e50-gpu.ts"
+    net = torch.jit.load(model)
     reply = None
     while True:
         request = worker.recv(reply)
@@ -24,14 +27,20 @@ def main():
         label = json.loads(m.label)
         shape = label["TENS"][0]["shape"]
         payload = m._payload[0]
-        ret = np.frombuffer(payload, dtype='f').reshape(shape)
-        print('input:\n', ret)
-        ret = ret*10
-        print('output:\n', ret)
+        img = np.frombuffer(payload, dtype='f').reshape(shape)
 
-        reply = zio.Message(form='TENS', label=m.label, 
+        img_tensor = torch.from_numpy(np.transpose(img, axes=[2, 0, 1])) # hwc_to_chw
+        img_tensor = img_tensor.cuda()
+        with torch.no_grad():
+          input = img_tensor.unsqueeze(0)
+          print ("input.shape: ", input.shape)
+          mask = net.forward(input).squeeze().cpu().numpy()
+          print('mask.shape:\n', mask.shape)
+
+        reply = zio.Message(form='TENS',
+              label=json.dumps({"TENS":[{"dtype":'f',"part":1,"shape":mask.shape,"word":4}]}), 
               level=zio.MessageLevel.warning,
-              payload=[ret.tobytes()]).toparts()
+              payload=[mask.tobytes()]).toparts()
 
 
 